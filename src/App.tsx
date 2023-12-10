35d0b42757115d74060a8c30e0089e4b45551a17
Denis Klyuev <denis.klyuev@gmail.com>
2023-12-10 02:57:22 -0500

    finally decoding

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,12 @@ import {useEffect, useRef, useState} from 'react';
 import {FFmpeg} from '@ffmpeg/ffmpeg';
 import {fetchFile, toBlobURL} from '@ffmpeg/util';
 
-const baseURL = 'https://unpkg.com/@ffmpeg/core-mt@0.12.4/dist/esm';
+interface ProgressEvent {
+    progress: number;
+    time: number;
+}
+
+const baseURL = 'https://unpkg.com/@ffmpeg/core@0.12.4/dist/esm';
 
 const usePrepareFfmpeg = (ffmpeg: FFmpeg) => {
     const [isLoaded, setIsLoaded] = useState(false);
@@ -12,6 +17,16 @@ const usePrepareFfmpeg = (ffmpeg: FFmpeg) => {
         (async () => {
             try {
                 console.log('loading ffmpeg...');
+                ffmpeg.on("log", ({message, type}) => {
+                    switch (type) {
+                        case 'stderr':
+                            console.error('log', message);
+                            break;
+                        default:
+                            console.log('log', message, type);
+                    }
+                });
+
                 await ffmpeg.load({
                     coreURL: await toBlobURL(`${baseURL}/ffmpeg-core.js`, 'text/javascript'),
                     wasmURL: await toBlobURL(`${baseURL}/ffmpeg-core.wasm`, 'application/wasm'),
@@ -28,6 +43,8 @@ const usePrepareFfmpeg = (ffmpeg: FFmpeg) => {
     return {isLoaded};
 };
 
+const templateFile = 'tinkoff_output.mp4';
+
 export const App = () => {
     const ffmpegRef = useRef(new FFmpeg());
     const videoRef = useRef<HTMLVideoElement>(null);
@@ -36,28 +53,39 @@ export const App = () => {
     const [decodingStatus, setDecodingStatus] = useState('');
     const {isLoaded: isFfmpegLoaded} = usePrepareFfmpeg(ffmpegRef.current);
     
-    console.log('isFfmpegLoaded', isFfmpegLoaded);
+    console.log('%cdecodingStatus', 'color: pink', decodingStatus);
 
     useEffect(() => {
         (async () => {
-            console.log(isDecoding, isFfmpegLoaded, ffmpegRef.current, videoRef.current);
             if (isDecoding || !isFfmpegLoaded || !ffmpegRef.current || !videoRef.current) {
                 console.log('return');
                 return;
             }
             
             const ffmpeg = ffmpegRef.current;
+            const updateDecodingStatus = ({progress, time}: ProgressEvent) => {
+                console.log('progress', progress, time);
+                setDecodingStatus(`converting video... ${(progress * 100).toFixed(2)}%`);
+            };
 
             console.log('start');
             setIsDecoding(true);
             setDecodingStatus('loading source video...');
-            const fetchedFile = await fetchFile('tinkoff_output.mp4');
+            const fetchedFile = await fetchFile(templateFile);
+            // videoRef.current.src = URL.createObjectURL(new Blob([fetchedFile.buffer], {type: 'video/mp4'}));
+            
             setDecodingStatus('feeding source video to ffmpeg...');
             await ffmpeg.writeFile('input.mp4', fetchedFile);
+            console.log(await ffmpeg.listDir('.'));
+            
             setDecodingStatus('converting video...');
+            ffmpeg.on("progress", updateDecodingStatus);
             await ffmpeg.exec(['-i', 'input.mp4', 'output.mp4']);
+            ffmpeg.off("progress", updateDecodingStatus);
+            
             setDecodingStatus('reading output video...');
             const data = await ffmpeg.readFile('output.mp4') as Uint8Array;
+            
             setDecodingStatus('previewing video...');
             videoRef.current.src = URL.createObjectURL(new Blob([data.buffer], {type: 'video/mp4'}));
             setIsDecoding(false);
@@ -68,7 +96,7 @@ export const App = () => {
         <div>
             <h4>Video meme generator app</h4>
             <div>Decoding status: {decodingStatus}</div>
-            <video ref={videoRef}></video>
+            <video controls={true} ref={videoRef}></video>
         </div>
     );
 };

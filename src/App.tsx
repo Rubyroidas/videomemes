3a86ecb3bbd93bedcbc3e0885fc41abd235d00ef
Denis Klyuev <denis.klyuev@gmail.com>
2023-12-09 21:01:57 -0500

    basic video loading logic

--- src/App.tsx
+++ src/App.tsx
@@ -2,67 +2,73 @@ import {useEffect, useRef, useState} from 'react';
 import {FFmpeg} from '@ffmpeg/ffmpeg';
 import {fetchFile, toBlobURL} from '@ffmpeg/util';
 
+const baseURL = 'https://unpkg.com/@ffmpeg/core-mt@0.12.7/dist/umd';
+
 const usePrepareFfmpeg = (ffmpeg: FFmpeg) => {
     const [isLoaded, setIsLoaded] = useState(false);
 
     // load ffmpeg itself
     useEffect(() => {
         (async () => {
-            await ffmpeg.load({
-                coreURL: await toBlobURL(`${baseURL}/ffmpeg-core.js`, 'text/javascript'),
-                wasmURL: await toBlobURL(`${baseURL}/ffmpeg-core.wasm`, 'application/wasm'),
-            });
-            setIsLoaded(true);
+            try {
+                console.log('loading ffmpeg...');
+                await ffmpeg.load({
+                    coreURL: await toBlobURL(`${baseURL}/ffmpeg-core.js`, 'text/javascript'),
+                    wasmURL: await toBlobURL(`${baseURL}/ffmpeg-core.wasm`, 'application/wasm'),
+                    workerURL: await toBlobURL(`${baseURL}/ffmpeg-core.worker.js`, 'text/javascript'),
+                });
+                console.log('loaded');
+                setIsLoaded(true);
+            } catch (e) {
+                console.error('not loaded', e);
+            }
         })();
     }, []);
     
     return {isLoaded};
 };
 
-const useLoadVideoBinary = (url: string) => {
-    const [data, setData] = useState<Blob | null>(null);
-    useEffect(() => {
-        fetch(url)
-            .then(res => res.blob())
-            .then(blob => {
-                setData(blob);
-            });
-    }, [url]);
-    return {data};
-};
-
-const baseURL = 'https://unpkg.com/@ffmpeg/core@0.12.4/dist/umd';
-
 export const App = () => {
-    const {data: videoTemplate} = useLoadVideoBinary('tinkoff_output.mp4');
-    const [isDecoding, setIsDecoding] = useState(false);
     const ffmpegRef = useRef(new FFmpeg());
-    const {isLoaded: isFfmpegLoaded} = usePrepareFfmpeg(ffmpegRef.current);
+    const videoRef = useRef<HTMLVideoElement>(null);
 
-    console.log('data', videoTemplate, isFfmpegLoaded);
+    const [isDecoding, setIsDecoding] = useState(false);
+    const [decodingStatus, setDecodingStatus] = useState('');
+    const {isLoaded: isFfmpegLoaded} = usePrepareFfmpeg(ffmpegRef.current);
     
+    console.log('isFfmpegLoaded', isFfmpegLoaded);
+
     useEffect(() => {
         (async () => {
-            if (!videoTemplate || isDecoding || !isFfmpegLoaded || !ffmpegRef.current) {
+            console.log(isDecoding, isFfmpegLoaded, ffmpegRef.current, videoRef.current);
+            if (isDecoding || !isFfmpegLoaded || !ffmpegRef.current || !videoRef.current) {
+                console.log('return');
                 return;
             }
             
             const ffmpeg = ffmpegRef.current;
 
+            console.log('start');
             setIsDecoding(true);
-            ffmpeg.on('log', ({ message }) => {
-                console.log('log', message);
-            });
-            const fetchedFile = await fetchFile(videoTemplate);
-            // await ffmpeg.load({
-            //    
-            // })
-            console.log(fetchedFile);
+            setDecodingStatus('loading source video...');
+            const fetchedFile = await fetchFile('tinkoff_output.mp4');
+            setDecodingStatus('feeding source video to ffmpeg...');
+            await ffmpeg.writeFile('input.mp4', fetchedFile);
+            setDecodingStatus('converting video...');
+            await ffmpeg.exec(['-i', 'input.mp4', 'output.mp4']);
+            setDecodingStatus('reading output video...');
+            const data = await ffmpeg.readFile('output.mp4') as Uint8Array;
+            setDecodingStatus('previewing video...');
+            videoRef.current.src = URL.createObjectURL(new Blob([data.buffer], {type: 'video/mp4'}));
             setIsDecoding(false);
         })();
-    }, [videoTemplate, isFfmpegLoaded]);
+    }, [isFfmpegLoaded]);
     
     return (
-        <div>Video meme generator app</div>
+        <div>
+            <h4>Video meme generator app</h4>
+            <div>Decoding status: {decodingStatus}</div>
+            <video ref={videoRef}></video>
+        </div>
     );
 };

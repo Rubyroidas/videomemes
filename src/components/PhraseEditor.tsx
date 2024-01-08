b22b7e180e4d0cba2516792ecf01c9f63905973c
Denis Klyuev <denis.klyuev@gmail.com>
2024-01-08 00:16:12 -0500

    rework phrase editor

--- src/components/PhraseEditor.tsx
+++ src/components/PhraseEditor.tsx
@@ -16,91 +16,48 @@ import {NavigationBar} from './NavigationBar';
 import {useStore} from '../store';
 // import {DebugImage} from './DebugImage';
 
-type PhraseEditorProps = {
+type PhrasesEditorProps = {
     disabled: boolean;
     userPhrases: UserPhrase[];
     onChange: (phrases: UserPhrase[]) => void;
 }
+type PhraseEditorProps = {
+    disabled: boolean;
+    userPhrase: UserPhrase;
+    onChange: (phrases: UserPhrase) => void;
+}
 
-export const PhrasesEditor: FC<PhraseEditorProps> = (props) => {
-    const {userPhrases, onChange, ...rest} = props;
-
-    const onProxyChange = (phrases: UserPhrase[]) => {
-        const escapedPhrases: UserPhrase[] = phrases.map(({text, ...ppp}) => {
-            if (!text) {
-                return {text, ...ppp};
-            }
-
-            return ({
-                ...ppp,
-                text: html2text(text),
-            });
-        });
-        onChange(escapedPhrases);
-    };
-
-    const phrases = userPhrases.map(({text, ...ppp}) => ({
-        ...ppp,
-        text: text
-            ? text.split('\n')
-                .map((line) => `<div>${escapeHTML(line)}</div>`)
-                .join('')
-            : ''
-    }));
-
-    return (
-        <PhraseEditor
-            userPhrases={phrases}
-            onChange={onProxyChange}
-            {...rest}
-        />
-    );
-};
-
-export const PhraseEditor: FC<PhraseEditorProps> = (props) => {
+export const PhrasesEditor: FC<PhrasesEditorProps> = (props) => {
     const {disabled, userPhrases, onChange} = props;
-    const store = useStore();
     const [phraseIndex, setPhraseIndex] = useState(0);
-    const handlePhraseChange = (e: ContentEditableEvent) => {
+    const store = useStore();
+
+    const onProxyChange = (phrase: UserPhrase) => {
         const result = [...userPhrases];
-        result[phraseIndex] = {
-            ...result[phraseIndex],
-            text: e.target.value,
+        const escapedPhrase: UserPhrase = {
+            ...phrase,
+            text: phrase.text
+                ? html2text(phrase.text)
+                : phrase.text,
         };
+        result[phraseIndex] = escapedPhrase;
+
         onChange(result);
-    };
-    const handlePaste: ClipboardEventHandler = (e) => {
-        e.preventDefault();
-        document.execCommand('inserttext', false, e.clipboardData.getData('text/plain'));
-    };
-    const handleVideoClick: MouseEventHandler = (e) => {
-        const video = e.target as HTMLVideoElement;
-        if (video.paused) {
-            video.play();
-        } else {
-            video.pause();
-        }
     };
 
     const userPhrase = userPhrases[phraseIndex];
-
-    const collection = store.collections!.find(c => c.id === userPhrase.collectionId)!;
-    const item = collection.items.find(item => item.id === userPhrase.phraseId)!;
-
+    const preparedUserPhrase = {
+        ...userPhrase,
+        text: userPhrase.text
+            ? userPhrase.text.split('\n')
+                .map((line) => `<div>${escapeHTML(line)}</div>`)
+                .join('')
+            : ''
+    };
     const canGoLeft = phraseIndex > 0;
     const canGoRight = phraseIndex < userPhrases.length - 1;
-
-    const virtualRect: Rect = {
-        x: collection.textArea.x / collection.size.width * 100,
-        y: collection.textArea.y / collection.size.height * 100,
-        width: collection.textArea.width / collection.size.width * 100,
-        height: collection.textArea.height / collection.size.height * 100,
-    };
-    const fontSizeDesktop = FONT_SIZE * collection.size.width;
-    const fontSizeMobile = FONT_SIZE * 100;
-    const paddingDesktop = TEXT_PADDING * collection.size.width / 100;
-    const paddingMobile = TEXT_PADDING;
-    const inputClassName = TextAreaClass(fontSizeDesktop, fontSizeMobile, paddingDesktop, paddingMobile, item.name);
+    const collection = store.collections!.find(c => c.id === userPhrase.collectionId)!;
+    const item = collection.items.find(item => item.id === userPhrase.phraseId)!;
 
     return (
         <div>
@@ -116,34 +73,81 @@ export const PhraseEditor: FC<PhraseEditorProps> = (props) => {
                     <b>{collection.name}</b> "{item.name}"
                 </NavigateCaption>
             </Header>
-            <div>
-                <EditingAreaContainer {...collection.size}>
-                    <InputBackground {...virtualRect}>
-                        {/* userPhrase.text && (
-                            <DebugImage
-                                collection={collection}
-                                text={userPhrase.text}
-                            />
-                        ) */}
-                        <ContentEditable
-                            onPaste={handlePaste}
-                            disabled={disabled}
-                            className={inputClassName}
-                            html={userPhrase.text!}
-                            onChange={handlePhraseChange}
+            <PhraseEditor
+                userPhrase={preparedUserPhrase}
+                disabled={disabled}
+                onChange={onProxyChange}
+            />
+        </div>
+    );
+};
+
+export const PhraseEditor: FC<PhraseEditorProps> = (props) => {
+    const {disabled, userPhrase, onChange} = props;
+    const store = useStore();
+
+    const handlePhraseChange = (e: ContentEditableEvent) => {
+        onChange({
+            ...userPhrase,
+            text: e.target.value,
+        });
+    };
+    const handlePaste: ClipboardEventHandler = (e) => {
+        e.preventDefault();
+        document.execCommand('inserttext', false, e.clipboardData.getData('text/plain'));
+    };
+    const handleVideoClick: MouseEventHandler = (e) => {
+        const video = e.target as HTMLVideoElement;
+        if (video.paused) {
+            video.play();
+        } else {
+            video.pause();
+        }
+    };
+
+    const collection = store.collections!.find(c => c.id === userPhrase.collectionId)!;
+    const item = collection.items.find(item => item.id === userPhrase.phraseId)!;
+
+    const virtualRect: Rect = {
+        x: collection.textArea.x / collection.size.width * 100,
+        y: collection.textArea.y / collection.size.height * 100,
+        width: collection.textArea.width / collection.size.width * 100,
+        height: collection.textArea.height / collection.size.height * 100,
+    };
+    const fontSizeDesktop = FONT_SIZE * collection.size.width;
+    const fontSizeMobile = FONT_SIZE * 100;
+    const paddingDesktop = TEXT_PADDING * collection.size.width / 100;
+    const paddingMobile = TEXT_PADDING;
+    const inputClassName = TextAreaClass(fontSizeDesktop, fontSizeMobile, paddingDesktop, paddingMobile, item.name);
+
+    return (
+        <div>
+            <EditingAreaContainer {...collection.size}>
+                <InputBackground {...virtualRect}>
+                    {/* userPhrase.text && (
+                        <DebugImage
+                            collection={collection}
+                            text={userPhrase.text}
                         />
-                    </InputBackground>
-                    <Video
-                        onClick={handleVideoClick}
-                        controls={false}
-                        loop={false}
-                        src={item.videoFile}
-                        disablePictureInPicture={true}
-                        disableRemotePlayback={true}
-                        controlsList="nofullscreen"
+                    ) */}
+                    <ContentEditable
+                        onPaste={handlePaste}
+                        disabled={disabled}
+                        className={inputClassName}
+                        html={userPhrase.text!}
+                        onChange={handlePhraseChange}
                     />
-                </EditingAreaContainer>
-            </div>
+                </InputBackground>
+                <Video
+                    onClick={handleVideoClick}
+                    controls={false}
+                    loop={false}
+                    src={item.videoFile}
+                    disablePictureInPicture={true}
+                    disableRemotePlayback={true}
+                    controlsList="nofullscreen"
+                />
+            </EditingAreaContainer>
         </div>
     );
 };

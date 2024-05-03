14acb2edc7913335f217d4924c0d892ae2c0c303
Denis Klyuev <denis.klyuev@gmail.com>
2024-05-02 20:42:11 -0400

    full switch off of analytics when in dev mode

--- src/services/analytics.ts
+++ src/services/analytics.ts
@@ -2,7 +2,9 @@ import * as amplitude from '@amplitude/analytics-browser';
 
 import {consoleLog} from '../utils';
 
-amplitude.init('2232c1e3d7a0f750d9304f61d86c1ef9');
+if (!__DEV__) {
+    amplitude.init('2232c1e3d7a0f750d9304f61d86c1ef9');
+}
 
 export const sendAnalyticsEvent = (event: Lowercase<string>, data: Record<Lowercase<string>, any> = {}) => {
     const page = window.location.pathname;

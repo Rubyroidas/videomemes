3cdd0be47741cd50ad77923e1c776eef60603223
Denis Klyuev <denis.klyuev@gmail.com>
2024-05-02 23:07:26 -0400

    analytics: app version tracking

--- src/services/analytics.ts
+++ src/services/analytics.ts
@@ -3,14 +3,18 @@ import * as amplitude from '@amplitude/analytics-browser';
 import {consoleLog} from '../utils';
 
 if (!__DEV__) {
-    amplitude.init('2232c1e3d7a0f750d9304f61d86c1ef9');
+    amplitude.init('2232c1e3d7a0f750d9304f61d86c1ef9', {
+        appVersion: __VER__,
+        defaultTracking: {
+            sessions: true,
+        },
+    });
 }
 
 export const sendAnalyticsEvent = (event: Lowercase<string>, data: Record<Lowercase<string>, any> = {}) => {
     const page = window.location.pathname;
     const eventParams: Record<Lowercase<string>, any> = {
         page,
-        build: __VER__,
         ...data,
     };
     consoleLog('sendAnalyticsEvent', event, eventParams);
